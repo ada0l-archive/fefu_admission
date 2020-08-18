759e7406a69c32be6d81c3269579c3cbca3304ec
ada0l <andreika.varfolomeev@yandex.ru>
2020-08-18 21:56:09 +1000

    refactoring

--- fefu_admission/university/applicants_holder/printer.py
+++ fefu_admission/university/applicants_holder/printer.py
@@ -9,24 +9,32 @@ class ApplicantsHolderInformationPrinter:
         self.applicants_holder = applicants_holder
 
     def print_info(self):
-        rows_list = [
-            ["Всего подало", self.applicants_holder.get_len_applicants()],
-            ["Бюджетников подало", self.applicants_holder.get_len_applicants(
-                type_of_completion=TypeOfCompletion.Budget)],
-            ["Бюджетные места на данный момент", self.applicants_holder.get_budget_places_for_now()],
-            ["Подало согласие", self.applicants_holder.get_len_applicants(agreement=True)],
-            ["Подало согласие(На общий основаниях)", self.applicants_holder.get_len_applicants(
-                type_of_completion=TypeOfCompletion.Budget, agreement=True)],
-        ]
         me_enrollee = self.applicants_holder.settings.me
+
+        len_applicants = self.applicants_holder.get_len_applicants()
+        budget_places = self.applicants_holder.places[TypeOfCompletion.Budget]
+        budget_places_for_now = self.applicants_holder.get_budget_places_for_now()
+
+        number_of_applicants = self.applicants_holder.get_len_applicants(type_of_completion=TypeOfCompletion.Budget)
+        number_of_applicants_with_agreement = self.applicants_holder.get_len_applicants(agreement=True)
+        rows_list = [
+            ["Название", self.applicants_holder.name],
+            ["Всего подало", len_applicants],
+            ["Бюджетных мест", budget_places],
+            ["Бюджетные места на данный момент", budget_places_for_now]
+        ]
         if me_enrollee is not None:
-            rows_list.append(
-                ["Мое место среди бюджетников", self.applicants_holder.get_position_of_me(TypeOfCompletion.Budget)])
-            rows_list.append(
-                ["Мое место среди бюджетников без высокобальников[250]",
-                 self.applicants_holder.get_position_of_me_without_high_scores(
-                     type_of_completion=TypeOfCompletion.Budget, agreement=False, high_scores=250)])
-            rows_list.append(["Мое место среди бюджетинков, подавших солгасие",
-                              self.applicants_holder.get_position_of_me(type_of_completion=TypeOfCompletion.Budget,
-                                                                        agreement=True)])
+            me_position_among_all = self.applicants_holder.get_position_of_me(TypeOfCompletion.Budget)
+            me_position_among_all_with_agreement = \
+                self.applicants_holder.get_position_of_me(type_of_completion=TypeOfCompletion.Budget, agreement=True)
+            rows_list += [
+                ["Мое место среди бюджетников", "{} из {} ({k:.3f})".format(me_position_among_all, number_of_applicants,
+                                                                            k=me_position_among_all / len_applicants)],
+                ["Мое место среди бюджетников подавших согласие", "{} из {} ({k:.3f})".format(
+                    me_position_among_all_with_agreement, number_of_applicants_with_agreement,
+                    k=me_position_among_all_with_agreement / budget_places_for_now)]
+            ]
+        else:
+            rows_list += [["Бюджетников подало", number_of_applicants]]
+        rows_list += [["Подало согласие", number_of_applicants_with_agreement]]
         print(tabulate(rows_list, tablefmt='fancy_grid'))
