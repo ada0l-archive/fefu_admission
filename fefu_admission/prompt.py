feaaa25e097c42520ad80aa721d44f7c640b718c
ada0l <andreika.varfolomeev@yandex.ru>
2020-08-19 13:15:59 +1000

    Prompt.get_choose()

--- fefu_admission/prompt.py
+++ fefu_admission/prompt.py
@@ -16,3 +16,17 @@ class Prompt:
     @staticmethod
     def get_str(message=""):
         return input("{}: ".format(message))
+
+    @staticmethod
+    def get_choose(selection_list=None):
+        if selection_list is None:
+            selection_list = []
+
+        for index, item in enumerate(selection_list):
+            print("{}. {}".format(index + 1, item))
+
+        selected_index = int(Prompt.get_str("Enter index"))
+        while not 1 <= selected_index <= len(selection_list):
+            selected_index = int(Prompt.get_str("Enter index"))
+
+        return selected_index
