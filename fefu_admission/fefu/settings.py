a4a22392112f3f529a8ad80e970fc70182366f25
ada0l <andreika.varfolomeev@yandex.ru>
2020-08-19 20:51:26 +1000

    fix fefu settings.py

--- fefu_admission/fefu/settings.py
+++ fefu_admission/fefu/settings.py
@@ -7,7 +7,7 @@ from lxml import html
 
 
 class FefuSettings(Settings):
-    URL_TABLE = "https://www.dvfu.ru/admission/spd/"
+    URL_TABLE = "https://www.dvfu.ru/admission/program-bs/b/"
 
     def __init__(self, university):
         super().__init__(university,
@@ -25,22 +25,36 @@ class FefuSettings(Settings):
     def __get_html_with_departments_list(self):
         return Utils.get_response(
             method="get",
-            url=self.URL_TABLE,
-            data={
-                "PROPERTY_1647": "Прием на обучение на бакалавриат/специалитет",
-                "PROPERTY_1677": "Бюджетная основа",
-                "PROPERTY_1648": "Очная",
-                "PROPERTY_1652": "Владивосток",
-                "PROPERTY_1642": self.default_settings_content["list_of_departments"][0]
-            }).text
+            url=self.URL_TABLE
+        ).text
 
     def get_list_of_all_departments(self):
+
+        def remove_spaces_on_the_sides(string):
+            start = 0
+            end = len(string)
+
+            for i in range(0, len(string)):
+                if string[i] != " " and string[i] != " " and string[i] != "\t":
+                    break
+                start += 1
+
+            for i in range(end - 1, -1, -1):
+                if string[i] != " " and string[i] != " " and string[i] != "\t":
+                    break
+                end -= 1
+            return string[start:end]
+
         page = html.fromstring(self.__get_html_with_departments_list())
-        form = page.get_element_by_id("sel5")
+        table = page.get_element_by_id("program")
+        tbody = table.find("tbody")
+        print(tbody)
 
         departments_list = []
-        for item in form:
-            item_str = item.text_content().replace("\t", "").replace("\n", "")
-            if item_str != "":
-                departments_list.append(item_str)
+        for tr in tbody[:len(tbody) - 1]:
+            department_str = "{} {}".format(
+                *[remove_spaces_on_the_sides(item.text_content().replace("\t", "").replace("\n", "")) for item in tr[0:2]]
+            )
+            departments_list.append(department_str)
+
         return departments_list
